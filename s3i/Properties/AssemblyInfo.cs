9ba5b24f3e3ed0bfca3ff8274b2dfad30a511ae4
Boulanov, Oleg <oleg.boulanov@gmail.com>
2019-06-28 14:17:43 -0400

    some info fields added

--- s3i/Properties/AssemblyInfo.cs
+++ s3i/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("s3i")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Msi installation helper")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Eliza Corporation")]
 [assembly: AssemblyProduct("s3i")]
 [assembly: AssemblyCopyright("Copyright © Oleg Boulanov 2019")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
