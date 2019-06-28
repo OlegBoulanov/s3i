a7a920f95fbcd2c82ec68e0c48efaf07c3d0f2f7
Boulanov, Oleg <oleg.boulanov@gmail.com>
2019-06-28 13:46:20 -0400

    Installer added

--- s3i/Properties/AssemblyInfo.cs
+++ s3i/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("s3i")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © Oleg Boulanov 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
