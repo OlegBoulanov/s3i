88ccfabce6ba4a5e0e697d4ac5f493fc481223ec
Boulanov, Oleg <oleg.boulanov@gmail.com>
2019-07-03 08:16:21 -0400

    Minor refactoring with no changes

--- s3i_lib/Products.cs
+++ s3i_lib/Products.cs
@@ -5,39 +5,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using System.Text.RegularExpressions;
 using System.IO;
 using System.Net;
 using Newtonsoft.Json;
-
-using s3i_lib;
 
 using Amazon.S3.Util;
 
 namespace s3i_lib
 {
-    public class ProductProps : Dictionary<string, string>
-    {
-    }
-    public class ProductInfo
-    {
-        public string Name { get; set; }
-        public string RelativeUri { get; set; }
-        public string AbsoluteUri { get; set; }
-        public string LocalPath { get; set; }
-        public HttpStatusCode DownloadResult { get; set; }
-        public ProductProps Props { get; protected set; } = new ProductProps();
-    }
-
     public class Products : List<ProductInfo>
     {
-        public Products()
-        {
-        }
-        public Products(IEnumerable<ProductInfo> p)
-        {
-            AddRange(p);
-        }
+        public Products() { }
+        public Products(IEnumerable<ProductInfo> p) { AddRange(p); }
         public string ToJson()
         {
             return JsonConvert.SerializeObject(this, Formatting.Indented);
