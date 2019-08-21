635ed133af0a39fb3d19e163cc30710ca70a4630
Boulanov, Oleg <oleg.boulanov@gmail.com>
2019-08-21 11:02:16 -0400

    Added s3i_service project

---
+++ s3i_service/ServiceS3i.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+using s3i_lib;
+
+namespace s3i_service
+{
+    public partial class ServiceS3i : ServiceBase
+    {
+        Process s3i_process;
+        public ServiceS3i()
+        {
+            InitializeComponent();
+            ServiceName = "s3i";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            var exeFilePath = new Uri(System.Reflection.Assembly.GetExecutingAssembly().CodeBase).AbsolutePath;
+            var s3i_path = $"{Path.GetDirectoryName(exeFilePath)}{Path.DirectorySeparatorChar}s3i.exe";
+            s3i_process = Process.Start(s3i_path, args.Aggregate("", (a, s) => { return $"{a} {s}"; }));
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
