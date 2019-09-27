a78393fd517d06e232e96859c746fee4411fa060
Boulanov, Oleg <oleg.boulanov@gmail.com>
2019-09-27 15:44:18 -0400

    First sketch of schedule

---
+++ s3i_lib/ScheduledTask.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace s3i_lib
+{
+    public class ScheduledTask<T>
+    {
+
+        public async Task<T> RunAsScheduled(Func<T> func, CancellationToken cancellationToken)
+        {
+            return await Task.Run(() => {
+                return func();
+            }, cancellationToken);
+        }
+
+    }
+}
