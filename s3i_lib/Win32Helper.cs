47e62d7a03774755667dda9dae02261103cf3853
Boulanov, Oleg <oleg.boulanov@gmail.com>
2019-10-08 09:07:11 -0400

    Win32Helper added

---
+++ s3i_lib/Win32Helper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace s3i_lib
+{
+    public class Win32Helper
+    {
+        public static string ErrorMessage(int code)
+        {
+            return new Win32Exception(code).Message;
+        }
+        public static string ErrorMessage()
+        {
+            return ErrorMessage(Marshal.GetLastWin32Error());
+        }
+    }
+}
