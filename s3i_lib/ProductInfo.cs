932aa85145077b839d391d8140b0c57207cf64e1
Boulanov, Oleg <oleg.boulanov@gmail.com>
2019-10-08 11:29:27 -0400

    Json serialization added and RelativeUri removed

--- s3i_lib/ProductInfo.cs
+++ s3i_lib/ProductInfo.cs
@@ -1,30 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using System.Net;
+using Newtonsoft.Json;
 
 namespace s3i_lib
 {
     public class ProductInfo
     {
         public string Name { get; set; }
-        public string RelativeUri { get; set; }
         public string AbsoluteUri { get; set; }
+        [JsonIgnore]
         public string LocalPath { get; set; }
         public ProductProps Props { get; protected set; } = new ProductProps();
-        public Installer.Action? CompareAndSelectAction(ProductInfo installedProduct)
+        public Installer.Action CompareAndSelectAction(ProductInfo installedProduct)
         {
             // use absolute uri to compare versions
-            var versionIsNewer = AbsoluteUri.CompareTo(installedProduct.AbsoluteUri);   // TODO: need more intelligence here...
+            var versionIsNewer = AbsoluteUri.CompareTo(installedProduct.AbsoluteUri);
             // if new is greater, install
             if (0 < versionIsNewer) return Installer.Action.Install;
             // else (if less or props changed) reinstall
             if (versionIsNewer < 0 || !Props.Equals(installedProduct.Props)) return Installer.Action.Reinstall;
             // else (if same and no props changed) do nothing
-            return null;
+            return Installer.Action.NoAction;
         }
+        #region Json Serialization
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+        public async Task ToJson(Stream stream)
+        {
+            using (var writer = new StreamWriter(stream))
+            {
+                await writer.WriteAsync(ToJson());
+            }
+        }
+        public static async Task<ProductInfo> FromJson(Stream stream)
+        {
+            using (var reader = new StreamReader(stream))
+            {
+                return JsonConvert.DeserializeObject<ProductInfo>(await reader.ReadToEndAsync());
+            }
+        }
+        public static string LocalInfoFileExtension { get; } = ".json";
+        public async Task SaveToLocal()
+        {
+            var path = $"{Path.GetFileNameWithoutExtension(LocalPath)}{LocalInfoFileExtension}";
+            using(var fs = new FileStream(path, FileMode.Create))
+            {
+                await ToJson(fs);
+            }
+        }
+        public static async Task<ProductInfo> FromLocal(string localPath)
+        {
+            using(var fs = new FileStream(localPath, FileMode.Open))
+            {
+                return await FromJson(fs);
+            }
+        }
+        #endregion
     }
 }
--- s3i_lib_tests/ProductInfo_Test.cs
+++ s3i_lib_tests/ProductInfo_Test.cs
@@ -12,5 +12,23 @@ namespace s3i_lib_tests
         public void Compare()
         {
         }
+        [TestMethod]
+        public void Serialization()
+        {
+            var pi = new ProductInfo { AbsoluteUri = "https://s3.something.com/install-me/me-me-me.msi", Name = "TestProduct",  };
+            pi.Props.Add("prop1", "value1");
+            pi.Props.Add("prop2", "value2");
+            var json = pi.ToJson().Replace("\r", "");
+            Assert.AreEqual(
+                "{\n"
+                + "  \"Name\": \"TestProduct\",\n"
+                + "  \"AbsoluteUri\": \"https://s3.something.com/install-me/me-me-me.msi\",\n"
+                + "  \"Props\": {\n"
+                + "    \"prop1\": \"value1\",\n"
+                + "    \"prop2\": \"value2\"\n"
+                + "  }\n"
+                + "}", 
+                json);
+        }
     }
 }
