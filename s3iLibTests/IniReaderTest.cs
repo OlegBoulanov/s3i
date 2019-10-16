4a1a80b76a3221a6769a7bcdf8088e0db199690d
Boulanov, Oleg <oleg.boulanov@gmail.com.com>
2019-10-16 16:48:30 -0400

    fxcop fixed

--- s3iLibTests/IniReaderTest.cs
+++ s3iLibTests/IniReaderTest.cs
@@ -17,21 +17,21 @@ namespace s3iLibTests
         public async Task ReadIniFromMemoryStream()
         {
             var sections = new Dictionary<string, Dictionary<string, string>>();
-            await IniReader.Read(
-                new MemoryStream(Encoding.ASCII.GetBytes("\n" +
-                "[$products$]\n" +
-                "Something = at this path; comment\n" +
-                "; comment this completely\n" +
-                "[Something]\n" +
-                "  prop1 =  value11 ; comment\n" +
-                "prop2 = value22\n" +
-                "prop3 = ;;; comment")),
+            using var stream = new MemoryStream(Encoding.ASCII.GetBytes("\n" +
+            "[$products$]\n" +
+            "Something = at this path; comment\n" +
+            "; comment this completely\n" +
+            "[Something]\n" +
+            "  prop1 =  value11 ; comment\n" +
+            "prop2 = value22\n" +
+            "prop3 = ;;; comment"));
+            await IniReader.Read(stream,
                 async (section, key, value) =>
                 {
                     if (!sections.ContainsKey(section)) sections.Add(section, new Dictionary<string, string>());
                     sections[section][key] = value;
-                    await Task.CompletedTask;
-                });
+                    await Task.CompletedTask.ConfigureAwait(false);
+                }).ConfigureAwait(false);
             Assert.AreEqual(2, sections.Count);
             Assert.AreEqual("value11", sections["Something"]["prop1"]);
             Assert.AreEqual("value22", sections["Something"]["prop2"]);
