361fa882fec47f952fc38d02805ce72ba70aa412
Oleg Boulanov <oleg.boulanov@gmail.com>
2019-10-18 10:48:42 -0400

    Warning scope reduced

--- s3iLibTests/ExceptionExtensionsTest.cs
+++ s3iLibTests/ExceptionExtensionsTest.cs
@@ -5,7 +5,6 @@ using NUnit.Framework;
 
 using s3iLib;
 
-#pragma warning disable CA1303  //warning CA1303: Method 'void ExceptionExtemsionsTest.ErrorMessage()' passes a literal string as parameter 'message' of a call to
 namespace s3iLibTests
 {
 
@@ -14,13 +13,14 @@ namespace s3iLibTests
         [Test]
         public void ErrorMessage()
         {
+#pragma warning disable CA1303  //warning CA1303: Method 'void ExceptionExtemsionsTest.ErrorMessage()' passes a literal string as parameter 'message' of a call to
             var x = new FileNotFoundException("bla-blah");
             Console.WriteLine($"? {x.Format()}");
             Assert.That(x.Format(), Is.EqualTo($"FileNotFoundException: bla-blah{Environment.NewLine}"));
             var y = new IOException("with inner!", new FileNotFoundException("none", new Exception("more")));
             Console.WriteLine($"? {y.Format(4)}");
             Assert.That(y.Format(2), Is.EqualTo($"IOException: with inner!{Environment.NewLine}  FileNotFoundException: none{Environment.NewLine}    Exception: more{Environment.NewLine}"));
+#pragma warning restore CA1303
         }
     }
 }
-#pragma warning restore CA1303
