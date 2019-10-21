d4ff408ad98f4529f5c0acbeebd336fb3f00bea0
Boulanov, Oleg <oleg.boulanov@gmail.com.com>
2019-10-21 12:22:32 -0400

    Lazy filed made readonly

--- s3iLib/DownloaderS3.cs
+++ s3iLib/DownloaderS3.cs
@@ -46,7 +46,7 @@ namespace s3iLib
         #region Static data and methods
         public static string ProfileName { get; set; }
         public static AmazonS3Client DefaultClient { get; set; }
-        static Lazy<AmazonS3ClientMap> ClientMap = new Lazy<AmazonS3ClientMap>(() =>
+        static readonly Lazy<AmazonS3ClientMap> ClientMap = new Lazy<AmazonS3ClientMap>(() =>
         {
             if (!new CredentialProfileStoreChain().TryGetAWSCredentials(ProfileName, out var credentials))
                 throw new ApplicationException($"Can't find AWS profile [{ProfileName}]");
