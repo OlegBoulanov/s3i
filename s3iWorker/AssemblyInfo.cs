bb5832202e2a0a7ab2d20780eda4c60a441d8188
Boulanov, Oleg <oleg.boulanov@gmail.com.com>
2019-10-23 13:53:33 -0400

    AssemblyInfo added

---
+++ s3iWorker/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("s3iw")]
+[assembly: AssemblyDescription("s3i worker")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("UselessExtras")]
+[assembly: AssemblyProduct("s3i msi package batch installer")]
+[assembly: AssemblyCopyright("Copyright © 2019, Oleg Boulanov")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")] // must be empty, allowed for satellite assemblies only
+
+[assembly: AssemblyVersion("1.2.3.0")]
+[assembly: AssemblyFileVersion("1.2.3.0")]
+[assembly: AssemblyInformationalVersion("1.2.3.0")]
