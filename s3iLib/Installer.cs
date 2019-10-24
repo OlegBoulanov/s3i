be0fb5a73c6f8d6e0d9010ee00634a3a1fe2fbc1
Oleg Boulanov <oleg.boulanov@gmail.com>
2019-10-24 17:26:43 -0400

    some linux improvements

--- s3iLib/Installer.cs
+++ s3iLib/Installer.cs
@@ -10,34 +10,41 @@ namespace s3iLib
     public static class Installer
     {
         public enum Action { NoAction, Install, Reinstall, Uninstall };
-        public static string MsiExec { get; set;  } = "msiexec.exe";
+        public static string MsiExec { get; set; } = "msiexec.exe";
         public static string InstallerFileExtension { get; set; } = ".msi";
         public static int RunInstall(string commandLineArgs, bool dryrun, TimeSpan timeout)
         {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) && !dryrun)
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
-                var args = $"/c {MsiExec.Quote(null)} {commandLineArgs}";
-                using (var process = Process.Start("cmd.exe", args))
+                if (!dryrun)
                 {
-                    if (timeout.TotalMilliseconds <= 0) return 0;
-                    if (false == process.WaitForExit((int)timeout.TotalMilliseconds) || 0 != process.ExitCode)
+                    var args = $"/c {MsiExec.Quote(null)} {commandLineArgs}";
+                    using (var process = Process.Start("cmd.exe", args))
                     {
-                        Console.WriteLine($"{MsiExec} failed with {process.ExitCode}");
+                        if (timeout.TotalMilliseconds <= 0) return 0;
+                        if (false == process.WaitForExit((int)timeout.TotalMilliseconds) || 0 != process.ExitCode)
+                        {
+                            Console.WriteLine($"{MsiExec} failed with {process.ExitCode}");
+                        }
+                        return process.ExitCode;
                     }
-                    return process.ExitCode;
+                }
+                else
+                {
+                    Console.WriteLine($"(DryRun) {MsiExec} {commandLineArgs}");
                 }
             }
             else
             {
-                Console.WriteLine($"{MsiExec} {commandLineArgs}");
-                return 0;
+                Console.WriteLine($"(Windows) {MsiExec} {commandLineArgs}");
             }
+            return 0;
         }
         public static string FormatCommand(string msiFilePath, ProductPropertiesDictionary props, string prefix, string suffix)
         {
             StringBuilder sb = new StringBuilder($"{prefix} {msiFilePath.Quote("\"")}");
             // begin with list of quoted if necessary props
-            if(null != props) sb.Append(props.Aggregate("", (s, a) => { s = $"{s} {a.Key}={a.Value.Quote("\"")}"; return s; }));
+            if (null != props) sb.Append(props.Aggregate("", (s, a) => { s = $"{s} {a.Key}={a.Value.Quote("\"")}"; return s; }));
             // now append extra args, so they may override props and set more msiexec options
             if (!string.IsNullOrEmpty(suffix)) sb.Append($" {suffix}");
             return sb.ToString();
