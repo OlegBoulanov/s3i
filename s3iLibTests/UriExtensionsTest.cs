83d46389a343e8dbe6e7b5b17a788ef9e5747f48
Oleg Boulanov <oleg.boulanov@gmail.com>
2019-10-24 19:19:42 -0400

    file: schema parsing added

--- s3iLibTests/UriExtensionsTest.cs
+++ s3iLibTests/UriExtensionsTest.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 
 using System.IO;
+using System.Diagnostics.Contracts;
 
 using s3iLib;
 
@@ -28,33 +29,43 @@ namespace s3iLibTests
             Assert.AreEqual("server.com", uri.Host);
             Assert.AreEqual(UriHostNameType.Dns, uri.HostNameType);
             Assert.AreEqual($"{Path.DirectorySeparatorChar}folder{Path.DirectorySeparatorChar}file.ext", uri.AbsolutePath);
+            Assert.Throws(typeof(FormatException), () => uri.GetAbsoluteFilePath());
             // File URI
             var fileUri = new Uri("file:///c:\\folder\\file.ext");
             Assert.AreEqual("file", fileUri.Scheme);
             Assert.AreEqual(string.Empty, fileUri.Host);
             Assert.AreEqual(UriHostNameType.Basic, fileUri.HostNameType);
             Assert.AreEqual($"c:{Path.DirectorySeparatorChar}folder{Path.DirectorySeparatorChar}file.ext", fileUri.AbsolutePath);
-            // UNC
-            var unc = new Uri("file:///\\\\server\\folder\\file.ext");
-            Assert.AreEqual("file", unc.Scheme);
-            Assert.AreEqual("server", unc.Host);
-            Assert.AreEqual(UriHostNameType.Dns, unc.HostNameType);
-            Assert.AreEqual($"{Path.DirectorySeparatorChar}folder{Path.DirectorySeparatorChar}file.ext", unc.AbsolutePath);        
+            Assert.AreEqual($"c:{Path.DirectorySeparatorChar}folder{Path.DirectorySeparatorChar}file.ext", fileUri.GetAbsoluteFilePath());
             // Local file
             var rootedFile = new Uri("c:\\folder/file.ext");
             Assert.AreEqual("file", rootedFile.Scheme);
             Assert.AreEqual(string.Empty, rootedFile.Host);
             Assert.AreEqual(UriHostNameType.Basic, rootedFile.HostNameType);
             Assert.AreEqual($"c:{Path.DirectorySeparatorChar}folder{Path.DirectorySeparatorChar}file.ext", rootedFile.AbsolutePath);
-            // UNC file
+            Assert.AreEqual($"c:{Path.DirectorySeparatorChar}folder{Path.DirectorySeparatorChar}file.ext", rootedFile.GetAbsoluteFilePath());
+            // UNC
+            var unc = new Uri("file:///\\\\server\\folder\\file.ext");
+            Assert.AreEqual("file", unc.Scheme);
+            Assert.AreEqual("server", unc.Host);
+            Assert.AreEqual(UriHostNameType.Dns, unc.HostNameType);
+            Assert.AreEqual($"{Path.DirectorySeparatorChar}folder{Path.DirectorySeparatorChar}file.ext", unc.AbsolutePath);        
+            Assert.AreEqual($"\\\\{unc.Host}{Path.DirectorySeparatorChar}folder{Path.DirectorySeparatorChar}file.ext", unc.GetAbsoluteFilePath());        
+           // UNC file
             var uncFile = new Uri("\\\\server\\folder\\file.ext");
             Assert.AreEqual("file", uncFile.Scheme);
             Assert.AreEqual("server", unc.Host);
             Assert.AreEqual(UriHostNameType.Dns, uncFile.HostNameType);
-            Assert.AreEqual($"{Path.DirectorySeparatorChar}folder{Path.DirectorySeparatorChar}file.ext", uncFile.AbsolutePath);     
+            Assert.AreEqual($"{Path.DirectorySeparatorChar}folder{Path.DirectorySeparatorChar}file.ext", uncFile.AbsolutePath);
+            Assert.AreEqual($"\\\\{unc.Host}{Path.DirectorySeparatorChar}folder{Path.DirectorySeparatorChar}file.ext", uncFile.GetAbsoluteFilePath());
             // Relative file is not a URI
-            Assert.Throws(typeof(System.UriFormatException), () => {
+            Assert.Throws(typeof(System.UriFormatException), () =>
+            {
                 var relativeFile = new Uri("folder\\file.ext");
+            });
+            Assert.Throws(typeof(System.UriFormatException), () =>
+            {
+                var relativeFile = new Uri("\\folder\\file.ext");
             });
         }
     }
