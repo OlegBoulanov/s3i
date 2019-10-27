c77f6db15210837ba030c8fca6a28439d1d6b549
Oleg Boulanov <oleg.boulanov@gmail.com>
2019-10-27 12:39:24 -0400

    More work needed incorporating last modified

--- s3iLib/ProductInfo.cs
+++ s3iLib/ProductInfo.cs
@@ -20,6 +20,7 @@ namespace s3iLib
 #pragma warning disable CA2227 // we need write access to deserialize
         public ProductPropertiesDictionary Props { get; set; } = new ProductPropertiesDictionary();
 #pragma warning restore CA2227
+        [JsonIgnore]
         public DateTimeOffset LastModified { get; set; }
         public string MapToLocalPath(string basePath)
         {
@@ -29,25 +30,25 @@ namespace s3iLib
         {
             return $"{Path.Combine(Path.Combine(basePath, productName), fileName)}";
         }
-        public InstallAction CompareAndSelectAction(ProductInfo installedProduct, params string [] prefixes)
+        public InstallAction CompareAndSelectAction(ProductInfo installedProduct, params string[] prefixes)
         {
-            if(null == installedProduct) return InstallAction.Install;
+            if (null == installedProduct) return InstallAction.Install;
             var versionIsNewer = Uri.CompareSemanticVersion(installedProduct.Uri, prefixes);
             // if new is greater, install
             if (0 < versionIsNewer) return InstallAction.Upgrade;
             // else (if less or props changed) reinstall
             if (versionIsNewer < 0) return InstallAction.Downgrade;
             // same version, changed props means reinstall
-            if(!Props.Equals(installedProduct.Props)) return InstallAction.Reinstall;
-            // compare modification times then and reinstall, if upstream has been updated since last installation
-            if(DateTimeOffset.MinValue < installedProduct.LastModified && installedProduct.LastModified < LastModified) return InstallAction.Reinstall;
+            if (!Props.Equals(installedProduct.Props)) return InstallAction.Reinstall;
+            //// compare modification times then and reinstall, if upstream has been updated since last installation
+            //if (DateTimeOffset.MinValue < installedProduct.LastModified && installedProduct.LastModified < LastModified) return InstallAction.Reinstall;
             // else (if same version, datetime, and no props changed) 
             return InstallAction.NoAction;
         }
         #region Json Serialization
         public string ToJson()
         {
-            return JsonSerializer.Serialize<ProductInfo>(this, new JsonSerializerOptions { WriteIndented = true } );
+            return JsonSerializer.Serialize<ProductInfo>(this, new JsonSerializerOptions { WriteIndented = true });
         }
         public async Task ToJson(Stream stream)
         {
