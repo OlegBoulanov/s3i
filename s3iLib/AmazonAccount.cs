862f68d564ce623a180b9002ecc9260518f827b5
Oleg Boulanov <oleg.boulanov@gmail.com>
2023-07-27 19:04:29 -0400

    Profile credentials lookup simplified

--- s3iLib/AmazonAccount.cs
+++ s3iLib/AmazonAccount.cs
@@ -17,34 +17,18 @@ namespace s3iLib
 {
     public static class AmazonAccount
     {
+        static ICredentialProfileSource credentialProfileSource => new SharedCredentialsFile();
+        // these can be set from CommandLine
         public static string RegionName { get; set; } = "us-east-1";
         public static string ProfileName { get; set; } = "default";
-        public static Lazy<AWSCredentials> Credentials { get; } = new Lazy<AWSCredentials>(() =>
+        public static Lazy<AWSCredentials> Credentials => new Lazy<AWSCredentials>(() =>
         {
-            if(string.IsNullOrWhiteSpace(ProfileName))
-            {
-                return new InstanceProfileAWSCredentials();
-            }
-            if (!new CredentialProfileStoreChain().TryGetProfile(ProfileName, out var profile))
-                throw new ApplicationException($"Can't find AWS profile [{ProfileName}]");
-            // we need to avoid unnecessary AssumeRole() if running on EC2 and the role is available
-            // see https://aws.amazon.com/blogs/security/announcing-an-update-to-iam-role-trust-policy-behavior/
-            string instanceRoleName = null;
-            try
-            {
-                var instanceRoles = InstanceProfileAWSCredentials.GetAvailableRoles();
-                instanceRoleName = instanceRoles?.FirstOrDefault(role => profile.Options.RoleArn.EndsWith(role));
-            }
-            catch (Exception)
-            {
-                // not running on EC2
-            }
-            var parts = profile.Options.RoleArn.Split('/');     // arn:aws:iam::1234567890:role/name
-            var roleName = 2 == parts.Length ? parts[1] : "";   // never null
-            return string.IsNullOrWhiteSpace(instanceRoleName) || !roleName.Equals(instanceRoleName)
-                ? profile.GetAWSCredentials(profile.CredentialProfileStore) // follow standard chain: https://docs.aws.amazon.com/sdk-for-net/v3/developer-guide/creds-assign.html
-                : new InstanceProfileAWSCredentials(instanceRoleName);      // use instance refreshed credentials directly (not assuming any roles)
+            if (credentialProfileSource.TryGetProfile(ProfileName, out var profile)
+            && AWSCredentialsFactory.TryGetAWSCredentials(profile, credentialProfileSource, out var credentials))
+                return credentials;
+            if ("default" != ProfileName)
+                throw new ApplicationException($"Can't locate profile [{ProfileName}]");
+            return new InstanceProfileAWSCredentials();
         });
-
     }
 }
